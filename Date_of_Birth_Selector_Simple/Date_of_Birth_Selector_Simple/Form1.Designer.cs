d81063b72b58ad771730358b56869a098926dcdf
Sambo Chea <sombochea100@gmail.com>
2016-11-25 21:48:11 +0700

    This project for simple pick up Date of Birth for our Project in C# of Setec Int (Group SN1). By SomboChea.

---
+++ Date_of_Birth_Selector_Simple/Date_of_Birth_Selector_Simple/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Date_of_Birth_Selector_Simple
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.cbMonth = new System.Windows.Forms.ComboBox();
+            this.cbDay = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbYear
+            // 
+            this.cbYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Location = new System.Drawing.Point(98, 82);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(121, 34);
+            this.cbYear.TabIndex = 0;
+            this.cbYear.SelectedIndexChanged += new System.EventHandler(this.cbYear_SelectedIndexChanged);
+            // 
+            // cbMonth
+            // 
+            this.cbMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMonth.FormattingEnabled = true;
+            this.cbMonth.Items.AddRange(new object[] {
+            "January",
+            "Febuary",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "Angust",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.cbMonth.Location = new System.Drawing.Point(288, 82);
+            this.cbMonth.Name = "cbMonth";
+            this.cbMonth.Size = new System.Drawing.Size(217, 34);
+            this.cbMonth.TabIndex = 1;
+            this.cbMonth.SelectedIndexChanged += new System.EventHandler(this.cbMonth_SelectedIndexChanged);
+            // 
+            // cbDay
+            // 
+            this.cbDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDay.FormattingEnabled = true;
+            this.cbDay.Location = new System.Drawing.Point(572, 82);
+            this.cbDay.Name = "cbDay";
+            this.cbDay.Size = new System.Drawing.Size(121, 34);
+            this.cbDay.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Year";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(360, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(602, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Day";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 205);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbDay);
+            this.Controls.Add(this.cbMonth);
+            this.Controls.Add(this.cbYear);
+            this.Name = "frmMain";
+            this.Text = "Date of Birth Selector";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.ComboBox cbMonth;
+        private System.Windows.Forms.ComboBox cbDay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Date_of_Birth_Selector_Simple/Date_of_Birth_Selector_Simple/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Date_of_Birth_Selector_Simple
+{
+    public partial class frmMain : Form
+    {
+        //get date & time from local machine.
+        private static DateTime dt = DateTime.Now;
+        //get current year;
+        private int currentYear = dt.Year;
+        //count run the first form
+        private int count = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            int startYear = currentYear - 200;
+            int endYear = currentYear + 200;
+
+            for (int i = startYear; i <= endYear; i++)
+                cbYear.Items.Add("" + i);
+
+            cbYear.SelectedItem = "" + currentYear;
+            cbMonth.SelectedIndex = dt.Month-1;
+            cbDay.SelectedItem = "" + dt.Day;
+
+        }
+
+        private void cbMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbDay.Items.Clear();
+            int month = cbMonth.SelectedIndex+1;
+            int year = int.Parse(cbYear.SelectedItem+"");
+        
+            int days = DateTime.DaysInMonth(year, month);
+            for(int i = 1; i <= days; i++)
+                cbDay.Items.Add("" + i);
+            cbDay.SelectedItem = "" + cbDay.SelectedItem;
+        }
+
+        private void cbYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (count > 0)
+            {
+                cbDay.Items.Clear();
+                int month = cbMonth.SelectedIndex + 1;
+                int year = int.Parse(cbYear.SelectedItem + "");
+
+                int days = DateTime.DaysInMonth(year, month);
+                for (int i = 1; i <= days; i++)
+                    cbDay.Items.Add("" + i);
+                cbDay.SelectedItem = "" + cbDay.SelectedItem;
+            }
+            count = 1;
+        }
+    }
+}
